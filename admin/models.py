e2d232dab07799cfca03b0f5dbdf44fa6911fadb
laxmanmishra <laxmanmishrabbk@gmail.com>
2019-09-26 00:59:53 +0530

    Fix adnin migration

--- admin/models.py
+++ admin/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    
- name = models.CharField(max_length="1000"),
- img = models.ImageField(upload_to="pics"),
- price = models.IntegerField(),
- description = models.TextField(),
+
+ name = models.CharField(max_length=100)
+ img = models.ImageField(upload_to='pics')
+ price = models.IntegerField(default=0)
+ description = models.TextField()
  status = models.BooleanField(default=True)
